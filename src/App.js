2fd09f85f7377303427ff3350aa34a693f088aa2
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-18 00:23:28 -0500

    got the plane in the scene

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import { OrbitControls, Box } from "drei";
 import { Canvas } from "react-three-fiber";
 import "./App.css";
+
+import Plane from "./components/plane";
+
 function App() {
   return (
     <div className="app">
       <Canvas>
+        <Plane />
         {/* Scene lighting */}
         <ambientLight intensity={0.2} />
         {/* //Moving spotlight using the position of mouse in the screen (for now is set permantly will add control with oculus controller)*/}
