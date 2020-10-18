90c1397520d13af74fbe4fa69ff81f383c4be6ae
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-18 11:20:06 -0500

    adding kick sound on right squeek

--- src/components/Controllers.js
+++ src/components/Controllers.js
@@ -1,13 +1,19 @@
 import React, { useCallback } from "react";
 import { useXREvent, useController } from "react-xr";
 import useSound from "use-sound";
+
+import kickSfx from "../sounds/808-kick-01.wav";
+
 const Controllers = () => {
   // const leftController = useController("left");
   // console.log(leftController);
 
   const [kickSound] = useSound(kickSfx);
 
-  const onSqueeze = useCallback(() => console.log("Squeezed"), []);
+  const onSqueeze = useCallback(() => {
+    console.log("squeeze");
+    kickSound();
+  }, []);
   useXREvent("squeeze", onSqueeze, { handedness: "right" });
 
   return <></>;
