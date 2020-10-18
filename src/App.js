529122102c765a34a46da54f3b20eb1b6953b242
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-18 00:18:11 -0500

    setting up the scene

--- src/App.js
+++ src/App.js
@@ -7,15 +7,25 @@ function App() {
     <div className="app">
       <Canvas>
         {/* Scene lighting */}
-        <ambientLight />
-        <spotLight />
+        <ambientLight intensity={0.2} />
+        {/* //Moving spotlight using the position of mouse in the screen (for now is set permantly will add control with oculus controller)*/}
+        <spotLight
+          intensity={0.6}
+          position={[2, 3, 5]}
+          penumbra={1}
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+          castShadow
+        >
+          <object3D position={[0, 0, 0]} />
+        </spotLight>
 
         {/* Controll camera with mouse */}
         <OrbitControls />
 
         {/* Some geometry */}
-        <Box>
-          <meshStandardMaterial color="#e23" />
+        <Box castShadow>
+          <meshStandardMaterial color="gray" />
         </Box>
       </Canvas>
     </div>
