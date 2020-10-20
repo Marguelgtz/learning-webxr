d189a363b4fb53381aec34c107be533977d0feae
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-20 12:49:31 -0500

    added both drumsticcks; need to add live controller position functionallity need to add collision detection

--- src/components/drumSticks.js
+++ src/components/drumSticks.js
@@ -5,20 +5,44 @@ const Drumsticks = (params) => {
   const leftController = useController("left");
   const rightController = useController("right");
   if (leftController) {
-    console.log(leftController.controller.position.x);
+    console.log(leftController.controller);
   }
   return (
     <>
+      {/* left */}
       {leftController ? (
         <Box
           castShadow
-          args={[0.04, 0.04, 0.5]}
-          position={[0, 0.7, -0.7]}
-          // rotation={[
-          //   leftController.controller.rotation.x,
-          //   leftController.controller.rotation.y,
-          //   leftController.controller.rotation.z,
-          // ]}
+          args={[0.02, 0.02, 0.5]}
+          position={[
+            leftController.controller.position.x,
+            leftController.controller.position.y,
+            leftController.controller.position.z,
+          ]}
+          rotation={[
+            leftController.controller.rotation.x,
+            leftController.controller.rotation.y,
+            leftController.controller.rotation.z,
+          ]}
+        >
+          <meshStandardMaterial color="#966F33" />
+        </Box>
+      ) : null}
+      {/* right */}
+      {rightController ? (
+        <Box
+          castShadow
+          args={[0.02, 0.02, 0.5]}
+          position={[
+            rightController.controller.position.x,
+            rightController.controller.position.y,
+            rightController.controller.position.z,
+          ]}
+          rotation={[
+            rightController.controller.rotation.x,
+            rightController.controller.rotation.y,
+            rightController.controller.rotation.z,
+          ]}
         >
           <meshStandardMaterial color="#966F33" />
         </Box>
