9b83a143fd5560e0406d573438f1e809f5632316
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-20 14:00:26 -0500

    live controller position fixed.. thanks @sniok

--- src/components/drumSticks.js
+++ src/components/drumSticks.js
@@ -4,59 +4,45 @@ import { useFrame } from "react-three-fiber";
 import { Box } from "drei";
 
 const Drumsticks = (params) => {
-  const ref = useRef(); // Reference for the box instance
-
   const leftController = useController("left");
   const rightController = useController("right");
 
+  //controller position data
+  const leftControllerData = useRef(); // Reference for the box instance
+  const rigthControllerData = useRef(); // Reference for the box instance
+
+  //useFrame so controller data updates each render
   useFrame(() => {
-    // Don't forget to check this, controller may be disconnected
-    if (leftController === undefined) return;
-
-    ref.current.position.copy(leftController.controller.position);
-    ref.current.rotation.copy(leftController.controller.rotation);
+    //thanks @sniok live positions now works like a charm
+    if (leftController) {
+      leftControllerData.current.leftPosition.copy(
+        leftController.controller.position
+      );
+      leftControllerData.current.rightRotation.copy(
+        leftController.controller.rotation
+      );
+    }
+    if (rightController) {
+      rigthControllerData.current.leftPosition.copy(
+        leftController.controller.position
+      );
+      rigthControllerData.current.rightRotation.copy(
+        leftController.controller.rotation
+      );
+    }
   });
-
-  console.log("position/rotation left :", ref);
-  // NEED LIVE CONTROLLER POSITION
 
   return (
     <>
       {/* left */}
       {leftController ? (
-        <Box
-          castShadow
-          args={[0.02, 0.02, 0.5]}
-          position={[
-            leftController.grip.position.x,
-            leftController.grip.position.y,
-            leftController.grip.position.z,
-          ]}
-          rotation={[
-            leftController.controller.rotation.x,
-            leftController.controller.rotation.y,
-            leftController.controller.rotation.z,
-          ]}
-        >
+        <Box castShadow args={[0.02, 0.02, 0.5]} ref={leftControllerData}>
           <meshStandardMaterial color="#966F33" />
         </Box>
       ) : null}
       {/* right */}
       {rightController ? (
-        <Box
-          castShadow
-          args={[0.02, 0.02, 0.5]}
-          position={[
-            rightController.controller.position.x,
-            rightController.controller.position.y,
-            rightController.controller.position.z,
-          ]}
-          rotation={[
-            rightController.controller.rotation.x,
-            rightController.controller.rotation.y,
-            rightController.controller.rotation.z,
-          ]}
-        >
+        <Box castShadow args={[0.02, 0.02, 0.5]} ref={rigthControllerData}>
           <meshStandardMaterial color="#966F33" />
         </Box>
       ) : null}
