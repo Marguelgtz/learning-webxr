ce403e5be69b55d799cb376e3765551781cda8ee
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-27 03:53:14 -0600

    time percentage formula done

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { useFrame } from "react-three-fiber";
 //
-const useTimer = (callback, { status, date }, timerTime) => {
+const useTimer = (callback, { status, startTime }, timerTime) => {
   useState;
 
   useFrame(async () => {
     // timerPercentage => 0 = 0%, 1 = 100%
-    const timerPercentage = date - date + timerTime * 1000;
+
+    const timerPercentage =
+      (Date.now() - startTime + timerTime) / startTime + timerTime;
     // console.log(hoverStatus.date - Date.now());
     // console.log(Date.now() - hoverStatus.date);
-    console.log("Time Left", Date.now() - date);
+    console.log("Time Left", currentTime - date);
     if (status) {
       // console.log(hoverStatus.date - Date.now());
-      if (date - Date.now() <= -timerTime * 1000) {
+      if (date - currentTime <= -timerTime * 1000) {
         await callback();
       }
     }
