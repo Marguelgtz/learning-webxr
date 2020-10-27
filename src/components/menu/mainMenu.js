dd729bf94d128ea23b5d30c2258e23068aac5aa3
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-27 01:00:03 -0600

    working on main menu functionality

--- src/components/menu/mainMenu.js
+++ src/components/menu/mainMenu.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Text } from "drei";
+import { Hover } from "react-xr";
 
-const mainMenu = () => {
+const MainMenu = () => {
+  const [hoverStatus, setHoverStatus] = useState(false);
+
+  const handleStartHover = (hovered) => {
+    //need hover timer (do something when hovering for x amount of time)
+    //is playing to true
+    console.log(hovered);
+    if (hovered) {
+      // tomSound
+      setHoverStatus(true);
+    } else {
+      setHoverStatus(false);
+    }
+  };
+
   return (
     <>
-      <Box castShadow args={[1, 0.4, 0.3]} position={[0, 1.5, -2]}>
-        <meshStandardMaterial color="#966F33" />
-        <Text position={[0, 0, 0.16]} color="#000" fontSize={0.18}>
-          Start
-        </Text>
-      </Box>
+      {/*  right now set to only hover because I am developing with android vr, need to add controller support */}
+      {/* NEED CONTROLLER SUPPORT! */}
+      <Hover onChange={handleStartHover}>
+        <Box castShadow args={[1, 0.4, 0.3]} position={[0, 1.5, -2]}>
+          <meshStandardMaterial color="#966F33" />
+          <Text
+            position={[0, 0, 0.16]}
+            color={hoverStatus ? "#000" : "blue"}
+            fontSize={0.18}
+          >
+            Start
+          </Text>
+        </Box>
+      </Hover>
     </>
   );
 };
 
-export default mainMenu;
+export default MainMenu;
