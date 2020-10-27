127ad9b06d9d5d97be70141042e72b647b6c50c7
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-26 19:55:26 -0600

    moved drumkit up by 3 on y axis

--- src/redux/drumsPosition/reducer.js
+++ src/redux/drumsPosition/reducer.js
@@ -2,27 +2,27 @@ import {} from "./actions";
 
 const initialState = {
   kick: {
-    position: [0.2, 0, -0.6],
+    position: [0.2, 3, -0.6],
     rotation: [Math.PI / 2, 0, 0],
   },
   snare: {
-    position: [-0.3, 0.3, -0.2],
+    position: [-0.3, 3.3, -0.2],
     rotation: [0, 0, 0],
   },
   tom1: {
-    position: [0, 0.7, -0.7],
+    position: [0, 3.7, -0.7],
     rotation: [0.7, 0, (12 * Math.PI) / 6],
   },
   tom2: {
-    position: [0.5, 0.7, -0.65],
+    position: [0.5, 3.7, -0.65],
     rotation: [0.7, 0, Math.PI / 15],
   },
   hihat: {
-    position: [-0.6, 0.7, -0.48],
+    position: [-0.6, 3.7, -0.48],
     rotation: [0, 0, 0],
   },
   leftCymbal: {
-    position: [-0.5, 1.3, -0.7],
+    position: [-0.5, 3.3, -0.7],
     rotation: [0.7, 0, (12 * Math.PI) / 6],
   },
 };
