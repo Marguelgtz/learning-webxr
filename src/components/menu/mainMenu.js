e57496a990ec35f37ff61b65bdd73b58d13de152
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-26 23:38:29 -0600

    created a simple 1 menu button

--- src/components/menu/mainMenu.js
+++ src/components/menu/mainMenu.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Box, Text } from "drei";
 
 const mainMenu = () => {
   return (
     <>
-      <></>
+      <Box castShadow args={[1, 0.4, 0.3]} position={[0, 1.5, -2]}>
+        <meshStandardMaterial color="#966F33" />
+        <Text position={[0, 0, 0.16]} color="#000" fontSize={0.18}>
+          Start
+        </Text>
+      </Box>
     </>
   );
 };
