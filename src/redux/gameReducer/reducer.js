4a2e00f5e3e886587210eddd9023bbb65d3e0ca0
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-26 21:32:38 -0600

    created files for game reducer; started game reducer setup

---
+++ src/redux/gameReducer/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  playStatus: false,
+  editingDrumPosition: false,
+  isRecording: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    // start / stop play status
+    default:
+      return state;
+  }
+};
