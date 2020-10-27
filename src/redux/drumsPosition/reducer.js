acc9761ffc05b3b31631bc59d95879e1bfb313c6
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-26 20:43:17 -0600

    drumkit position corrected

--- src/redux/drumsPosition/reducer.js
+++ src/redux/drumsPosition/reducer.js
@@ -2,27 +2,27 @@ import {} from "./actions";
 
 const initialState = {
   kick: {
-    position: [0.2, 1, -0.6],
+    position: [0.1, 0.5, -1.1],
     rotation: [Math.PI / 2, 0, 0],
   },
   snare: {
-    position: [-0.3, 1.3, -0.2],
+    position: [-0.4, 0.8, -0.7],
     rotation: [0, 0, 0],
   },
   tom1: {
-    position: [0, 1.7, -0.7],
+    position: [-0.1, 1.2, -1.2],
     rotation: [0.7, 0, (12 * Math.PI) / 6],
   },
   tom2: {
-    position: [0.5, 1.7, -0.65],
+    position: [0.4, 1.2, -1.15],
     rotation: [0.7, 0, Math.PI / 15],
   },
   hihat: {
-    position: [-0.6, 1.7, -0.48],
+    position: [-0.5, 1.2, -0.98],
     rotation: [0, 0, 0],
   },
   leftCymbal: {
-    position: [-0.5, 2.3, -0.7],
+    position: [-0.4, 1.8, -1.2],
     rotation: [0.7, 0, (12 * Math.PI) / 6],
   },
 };
