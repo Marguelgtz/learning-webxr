a9557768ac186356987961917883851bd38165fb
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-26 22:01:50 -0600

    combined reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import reducer from "./redux/drumsPosition/reducer";
+import drumsPosition from "./redux/drumsPosition/reducer";
+import gameReducer from "./redux/gameReducer/reducer";
+
+const reducer = combineReducers({
+  drumsPosition: drumsPosition,
+  gameStatus: gameReducer,
+});
 
 const newStore = createStore(reducer, applyMiddleware(thunk));
 
