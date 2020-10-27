979a0e9c27bb161167ce3bbf6465d17db12df51d
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-10-27 04:21:55 -0600

    useTimer almost done

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -2,24 +2,42 @@ import React, { useState } from "react";
 import { useFrame } from "react-three-fiber";
 //
 const useTimer = (callback, { status, startTime }, timerTime) => {
-  useState;
-
+  const [timerStatus, setTimerStatus] = useState("not-running");
   useFrame(async () => {
     // timerPercentage => 0 = 0%, 1 = 100%
 
+    // timerTime (tT)
+    // startTime (sT)
+    // currentTime [Date.now] (cT)
+    // finalTime (fT) == startTime + timerTime
+    // currentTimeLeft (cTl) == currentTime - finalTime
+    // timerPercentage (Tp)
+    //
+    //    Tp = (ct - ft) / sT + tT ==> Tp = cTl / tT
+    //
+    //    timerPercentage =
+    // currentTimeLeft / finalTime
+
+    //    timerPercentage =  (currentTime - finalTime) / (startTime - timerTime )
+    //
+    //
+    //
+
     const timerPercentage =
-      (Date.now() - startTime + timerTime) / startTime + timerTime;
+      (Date.now() - (startTime + timerTime)) / startTime + timerTime;
+
     // console.log(hoverStatus.date - Date.now());
     // console.log(Date.now() - hoverStatus.date);
     console.log("Time Left", currentTime - date);
     if (status) {
+      setTimerStatus("running");
       // console.log(hoverStatus.date - Date.now());
       if (date - currentTime <= -timerTime * 1000) {
         await callback();
       }
     }
-    return { timerLeft, callbackFire };
+    return { timerPercentage, timerStatus };
   });
 };
-c;
+
 export default useTimer;
